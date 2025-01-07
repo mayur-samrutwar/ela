ce72ad414c2277d5105f35c35b207942ba6ac0ac
Mayur Samrutwar <samrutwarmayur1@gmail.com>
2025-01-08 04:08:36 +0530

    update: analytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,25 +13,20 @@ export default function RootLayout({children}: Readonly<{ children: React.ReactN
     return (
         <html lang="en" className={"h-full w-full"}>
             <head>
-                <Script id="google-tag-manager" strategy="afterInteractive">
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=G-10NHE5L6PV"
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
                     {`
-                        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                        })(window,document,'script','dataLayer','GTM-KGDWGDSQ');
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-10NHE5L6PV');
                     `}
                 </Script>
             </head>
             <body className={`antialiased w-full h-full flex flex-col`}>
-                <noscript>
-                    <iframe 
-                        src="https://www.googletagmanager.com/ns.html?id=GTM-KGDWGDSQ"
-                        height="0" 
-                        width="0" 
-                        style={{display: 'none', visibility: 'hidden'}}
-                    />
-                </noscript>
                 <div className="flex flex-col flex-grow w-full items-center justify-center px-2 sm:px-4">
                     <nav className="fixed w-full top-0 left-0 grid grid-cols-2 py-2 sm:py-4 px-4 sm:px-8 bg-background/80 backdrop-blur-sm z-10">
                         <div className="flex">
